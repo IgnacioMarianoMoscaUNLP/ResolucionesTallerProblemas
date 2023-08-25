9dba73b9cb2ca8e207f5c6a46dd308ee5652c590
ignacio mariano <ignaciomarianomosca.com>
2023-08-25 11:13:50 -0300

    hartals

--- hartals.py
+++ hartals.py
@@ -8,16 +8,17 @@ def num_hartals(parties, days):
                 days_with_hartals.add(day)
     return len(days_with_hartals)
 
-sys.stdout.write("Ingrese la cantidad de casos: ")
+
 T = int(sys.stdin.readline().strip())
 cases = []
 
 for _ in range(T):
-    sys.stdout.write("Ingrese la cantidad de días del caso: ")
+    
     N = int(sys.stdin.readline().strip())
-    sys.stdout.write("Ahora ingresará los días por partido en una sola línea: ")
+   
     number = sys.stdin.readline().strip()
     parties = list(map(int, number))
+    print(parties)
     cases.append(num_hartals(parties, N))
 
 for i in cases:
